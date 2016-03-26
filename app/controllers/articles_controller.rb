42d8c285f6bb62123feae02542049dd65d7a8865
Randall Lisy <lisyrj@gmail.com>
2016-03-26 03:46:46 +0000

    added partials and refactored

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def new
     @article = Article.new
   end
@@ -15,15 +15,15 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+    
   end
   
   def edit
-    @article = Article.find(params[:id])
+   
   end
   
   def update
-    @article = Article.find(params[:id])
+    
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -35,6 +35,14 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  
+  def destroy
+    
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+  
   
 
   private
